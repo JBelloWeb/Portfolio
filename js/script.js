001a06c315346c909cb52d38b65688b6939887c5
JBelloWeb <juanperalbello@gmail.com>
2026-02-23 20:51:29 -0300

    cards reorder

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const d = document;
 let bars = d.getElementById("bars");
 let ul = d.querySelector("ul");
+let tarjetas = d.querySelectorAll(".tarjeta");
 
 bars.addEventListener("click", () => {
     bars.className = "fa-solid fa-bars unactive";
@@ -12,3 +13,34 @@ d.addEventListener("click", (e) => {
         bars.className = "fa-solid fa-bars";
         ul.className = "disactive";
 }});
+
+const ReorderCards = () => {
+    let i = 0;
+    for(let t of tarjetas){
+        if(i == 0){t.className = "tarjeta top-card";}
+        if(i == 1){t.className = "tarjeta middle-card";}
+        if(i == 2){t.className = "tarjeta last-card";}
+        i++;
+    }
+}
+
+ReorderCards();
+
+const CardToBack = () => {
+    let firstCard = tarjetas[0];
+    tarjetas = Array.from(tarjetas);
+    tarjetas.shift();
+    tarjetas.push(firstCard);
+    ReorderCards();
+}
+
+for(let t of tarjetas){
+    t.addEventListener("click", CardToBack);
+}
+
+
+
+
+
+
+
